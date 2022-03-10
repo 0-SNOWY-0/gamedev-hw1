d0b080adf5ccda3130fbcf56321aecd6cdb2e69a
0-SNOWY-0 <snowboy1935@gmail.com>
2022-03-10 22:44:03 +0300

    حل الواجب
    👍

---
+++ homework_1/homework_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int health = 100;
+            string hero = "baby yoda";
+
+            int damaged() {
+                return health - 20;
+            }
+            int playerhp = damaged();
+
+            Console.WriteLine("Player HP after damage is :" + playerhp);
+
+            int boost() {
+                return playerhp + 10;
+            }
+
+            int player_hp = boost();
+
+            Console.WriteLine("Player HP after boost is :" + player_hp);
+
+            int[] HealthArray = { playerhp, player_hp };
+
+            Console.WriteLine(HealthArray[1]);
+
+            if (playerhp < player_hp) ;
+
+            Console.WriteLine("80 is less than 90");
+
+        }
+    }
+}
